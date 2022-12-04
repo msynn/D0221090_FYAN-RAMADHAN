9d9def14c60937f1fab3e0e5cdb3b230f2fc718e
msynn <fyanramadhan1@gmail.com>
2022-12-05 01:30:00 +0700

    Tugas3

---
+++ TugasBangunDatar.py
@@ -0,0 +1,24 @@
+# Tugas PBO : Menghitung luas bidang datar
+# 1. Persegi
+# 2. Lingkaran
+# 3. Segitiga
+
+# Nama  : Fyan Ramadhan
+# NIM   : D0221090
+# Kelas : Teknik Informatika F
+
+def garis1():
+    print('-'*42)
+    
+def garis2():
+    print('='*42)
+
+menu = ["1. Menghitung Luas Persegi","2. Menghitung Luas Lingkaran","3. Menghitung Luas Segitiga"]
+    
+garis2()
+print("Program Menghitung Luas Bidang Datar".center(42,' '))
+garis2()
+
+for i in menu:
+    print(i)
+
